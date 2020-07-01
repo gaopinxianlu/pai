b2054643ff284b8e0a6bcb53b7701c4fab34f3a0
pany <pany@landicorp.com>
2020-07-01 16:54:49 +0800

    pai框架增加viewbinding

--- app/src/main/java/cn/sc/pai/test/main/fragment/MainFragment.java
+++ app/src/main/java/cn/sc/pai/test/main/fragment/MainFragment.java
@@ -1,14 +1,9 @@
 package cn.sc.pai.test.main.fragment;
 
-import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.ViewGroup;
-
-import androidx.viewbinding.ViewBinding;
 
 import cn.pai.mvp.presenter.IPresenter;
 import cn.pai.mvp.view.IView;
-import cn.pai.mvp.view.PaiActivity;
 import cn.pai.mvp.view.PaiFragment;
 import cn.sc.pai.test.databinding.FragmentMainBinding;
 
